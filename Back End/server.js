98c6075bb659bdfad5f11f943030255ac2538dde
kieranmachale <c16777089@mytudublin.ie>
2022-01-19 16:58:43 +0000

    set cors options

--- Back End/server.js
+++ Back End/server.js
@@ -2,20 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const server = express();
 const PORT = process.env.PORT || 3000;
+const url = process.env.URL || "http://localhost";
 
-// cors settings
+// ---------------------------------------------------------------
+
+/* cors settings */
 let corsOptions = {
-    origin: '*'
+    origin: url + ":" + 8080
 }
 server.use(cors(corsOptions));
 
-// Endpoint to check server is active
+/* Endpoint to check server is active */
 server.get("/", cors(), (req, res) => {
     res.json('Server is running!').status(200);
 })
 
 /* Start server */
-server.listen(port, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });
 
